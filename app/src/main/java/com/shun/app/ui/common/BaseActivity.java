0b9501b7d6e507d8978c29d38ea5b9d820c652c2
Shun <shun@users.noreply.github.com>
2016-03-20 17:22:56 +0000

    Move the responsibility of showing movie details to the base activity

--- app/src/main/java/com/shun/app/ui/common/BaseActivity.java
+++ app/src/main/java/com/shun/app/ui/common/BaseActivity.java
@@ -1,20 +1,21 @@
 package com.shun.app.ui.common;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import com.shun.app.App;
 import com.shun.app.AppComponent;
+import com.shun.app.domain.models.MediaItem;
+import com.shun.app.ui.details.DetailsActivity;
+import com.shun.app.ui.events.ShowMediaItemDetails;
 import javax.inject.Inject;
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 public abstract class BaseActivity extends Activity {
   @Inject protected EventBus eventBus;
-
-  protected boolean shouldRegisterEventBus() {
-    return false;
-  }
 
   @Override @CallSuper protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -29,19 +30,27 @@ public abstract class BaseActivity extends Activity {
   @Override protected void onStart() {
     super.onStart();
 
-    if (shouldRegisterEventBus() && eventBus != null) {
+    if (eventBus != null) {
       eventBus.register(this);
     }
   }
 
   @Override protected void onStop() {
-    if (shouldRegisterEventBus() && eventBus != null) {
+    if (eventBus != null) {
       eventBus.unregister(this);
     }
 
     super.onStop();
   }
 
+  @Subscribe public void onShowMediaItemDetails(ShowMediaItemDetails event) {
+    MediaItem mediaItem = event.getMediaItem();
+
+    if (mediaItem != null) {
+      Intent intent = DetailsActivity.createIntent(this, mediaItem);
+      startActivity(intent);
+    }
+  }
   protected void setupComponent(@NonNull AppComponent appComponent) {
     appComponent.inject(this);
   }
